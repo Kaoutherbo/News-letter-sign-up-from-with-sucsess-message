f4ad9fa3ab975609c648ef7c1b48096c01af1cac
kaouther <kaoutherbouhteldjagmail.com>
2023-05-28 17:34:18 +0100

    Update size

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
-//For the input email
-    const submit = document.getElementById('submitbtn');
-    const Firstpage = document.getElementById('card');
-    const Secondpage = document.getElementById('card2');
-    const Inputemail = document.getElementById('InputEmail');
-    const submit2=document.getElementById('btn');
 
-    submit.addEventListener('click', ValidateEmail);
-    
+const submit = document.getElementById('submitbtn');
+const Firstpage = document.getElementById('card');
+const Secondpage = document.getElementById('card2');
+const Inputemail = document.getElementById('InputEmail');
+const submit2=document.getElementById('btn');
 
+submit.addEventListener('click', ValidateEmail);
+
+
+Secondpage.style.visibility = 'hidden';
+
+let mailformat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+function errorState(input, errorMessage) {
+  input.classList.add('error');
+  const small = input.parentElement.querySelector('small');
+  small.textContent = errorMessage;
+  const placeholderMessage='ash#loremcompany.com';
+    input.placeholder=placeholderMessage;
+
+}
+
+function removeErrorState(input) {
+    input.classList.remove('error');
+    const small = input.parentElement.querySelector('small');
+    small.textContent = '';
+  }
+
+function ValidateEmail(e) {
+  e.preventDefault();
+
+  if (Inputemail.value.match(mailformat)) {
+    Secondpage.style.visibility = 'visible';
+    Firstpage.style.visibility = 'hidden';
+    removeErrorState(Inputemail);
+  } else {
+    errorState(Inputemail, 'Valid email required');
     Secondpage.style.visibility = 'hidden';
+  }
+}
+submit2.addEventListener('click', ReturnToFirstPge);
+function  ReturnToFirstPge(e)  { 
+  e.preventDefault();  
+  Secondpage.style.visibility = 'hidden';
+  Firstpage.style.visibility = 'visible';
 
-    let mailformat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-
-    function errorState(input, errorMessage) {
-      input.classList.add('error');
-      const small = input.parentElement.querySelector('small');
-      small.textContent = errorMessage;
-      const placeholderMessage='ash#loremcompany.com';
-        input.placeholder=placeholderMessage;
-
-    }
-
-    function removeErrorState(input) {
-        input.classList.remove('error');
-        const small = input.parentElement.querySelector('small');
-        small.textContent = '';
-      }
-
-    function ValidateEmail(e) {
-      e.preventDefault();
-
-      if (Inputemail.value.match(mailformat)) {
-        Secondpage.style.visibility = 'visible';
-        Firstpage.style.display = 'none';
-        removeErrorState(Inputemail);
-      } else {
-        errorState(Inputemail, 'Valid email required');
-        Secondpage.style.visibility = 'hidden';
-      }
-    }
-    submit2.addEventListener('click', ReturnToFirstPge);
-    function  ReturnToFirstPge(e)  { 
-      e.preventDefault();  
-      Secondpage.style.visibility = 'hidden';
-      Firstpage.style.display = 'block';
-
-    }
-    
+}
